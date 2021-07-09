adc11a63b510fd2bce6c90e2aa51d3354eff32dc
Bobstuna <qwertster0@gmail.com>
2021-07-09 15:39:19 +1200

    got basic portal render texture and camera working

--- Assets/Portal/Portal.cs
+++ Assets/Portal/Portal.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Portal LinkedTo;
+
+    [SerializeField]
+    private MeshRenderer _render;
+
+    [SerializeField]
+    private Camera _portalCam;
+
+    [SerializeField]
+    private RenderTextureHolder _baseRT;
+
+    [SerializeField]
+    private Material _basePortalMat;
+
+    private RenderTexture _renderTexture;
+
+	private void OnEnable()
+	{
+        if (!_renderTexture)
+            _renderTexture = new RenderTexture(_baseRT.RT);
+
+        _portalCam.targetTexture = _renderTexture;
+
+        _render.material = new Material(_basePortalMat);
+        _render.material.mainTexture = _renderTexture;
+	}
+
+	private void OnDisable()
+	{
+        _portalCam.targetTexture = null;
+
+        _render.material = null;
+
+        _renderTexture.Release();
+
+        _renderTexture = null;
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void LateUpdate()
+	{
+        _portalCam.Render();
+	}
 }
---
+++ Assets/Render Texture Crap/RenderTextureHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RenderTextureHolder", menuName = "Other/Render Texture Holder")]
+public class RenderTextureHolder : ScriptableObject
+{
+	public RenderTexture RT;
+}
